736f933b18efdb9fb821e9a655af8278f3c17b0e
likhith-D3vCode <likhithnagavelliwebdev@gmail.com>
2024-11-03 23:17:38 +0530

    Revert "Added SignUp and Login Page with JWT Authentication and arrenged the …"

--- Back-End/routers/signupRouter.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const SignupRouter=express.Router();
-const {handleUserSignup}=require("../controllers/handleSignup");
-SignupRouter.post("/user",handleUserSignup);
-
-
-module.exports=SignupRouter;
